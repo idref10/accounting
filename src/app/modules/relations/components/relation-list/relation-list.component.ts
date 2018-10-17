3903a515805f6b2fe46af869a01343330d27f1a2
Ferdi Louwerse <ferdi.louwerse@partner.alphabet.com>
2018-10-17 14:36:56 +0200

    remove comments

--- src/app/modules/relations/components/relation-list/relation-list.component.ts
+++ src/app/modules/relations/components/relation-list/relation-list.component.ts
@@ -39,15 +39,6 @@ export class RelationListComponent implements OnInit {
   loadRelationsLazy(event: LazyLoadEvent) {
     if(!this.initialLoading) {
 
-      //in a real application, make a remote request to load data using state metadata from event
-      //event.first = First row offset
-      //event.rows = Number of rows per page
-      //event.sortField = Field name to sort with
-      //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
-      //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
-
-      //imitate db connection over a network
-
       this.loading = true;
 
       this.relationService.getFiltered(event.first).subscribe(response => {
